6b304383b11f83d6d8658463642e97a6b9441986
IT-Wizard\SuYin <suyin2096@gmail.com>
2024-02-19 15:21:58 +0630

    initial commit

---
+++ src/test/java/com/su/rabbitmq/springbootrabbitmq/SpringbootRabbitmqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.rabbitmq.springbootrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootRabbitmqApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
